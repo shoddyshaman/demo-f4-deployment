85fc60cc05ffcacfcde1e596967eb157b261aee8
shoddyshaman <djtiwari90@gmail.com>
2021-07-29 11:17:19 -0700

    added endpoint get

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const path = require('path');
 
 const app = express()
 
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
 const port = process.env.PORT || 5500
 
 app.listen(port ,() => { console.log(`Listening on port ${port}`)})
